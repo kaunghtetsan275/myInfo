c86ea0c12ee5d719020ac63b8c086e83b6de48bf
Oishi Sakana <kaunghtetsan275@gmail.com>
2024-02-15 17:34:20 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -104,6 +104,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+            Kevin trying to deploy it in live production
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
